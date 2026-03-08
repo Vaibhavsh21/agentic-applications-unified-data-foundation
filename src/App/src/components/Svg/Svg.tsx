51db41c6f48ff8bc931e534e5d32efb0c56c73e3
Your Namesomeone <someone@someplace.com>
2026-03-08 14:55:45 +0530

    Initial commit

---
+++ src/App/src/components/Svg/Svg.tsx
@@ -0,0 +1,16 @@
+import { Avatar } from "@fluentui/react-components";
+
+export const AppLogo = () => {
+  return (
+    <img
+      src="https://lankainformation.lk/media/com_mtree/images/listings/m/29916.jpg"   // put logo in public folder
+      alt="Ultratech Cement"
+      style={{
+        height: "40px",
+        width: "auto",
+        objectFit: "contain",
+      }}
+    />
+  );
+};
+
